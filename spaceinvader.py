6c1c1e66ab3c9337cba7958831f253ab72523ef6
khalid282 <khalidsodunke@gmail.com>
2025-12-01 11:06:58 +0000

    i used a tutorial known as kidscancode to know what to do but i made some changes to the code used in the tutorial

--- spaceinvader.py
+++ spaceinvader.py
@@ -17,13 +17,13 @@ class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load("defender.png").convert()
-        self.image = pg.transform.scale(self.image, (50, 38))
+        self.image = pg.transform.scale(self.image, (50, 38))#to generate the size of the player
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH/2. #player spawning point
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
-        self.lives = 3
-        self.hidden = False
+        self.lives = 3 #number of lives
+        self.hidden = False #to check if the player is hidden when hit by invader bullet
         self.hide_timer = 0
 
     def update(self):
@@ -38,8 +38,8 @@ class Player(pg.sprite.Sprite):
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH #prevents the player from going out side the screen
         if self.rect.left < 0:
-            self.rect.left = 0 
-    
+            self.rect.left = 0
+
 
     def shoot(self):
         bullet = Bullet(self.rect.centerx, self.rect.top)
