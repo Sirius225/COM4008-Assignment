27a8fc437d50900ecd6062d0acbb28ea8c5ad474
khalid282 <khalidsodunke@gmail.com>
2025-12-02 15:21:27 +0000

    created a function that draws text to the screen of the game. I plan on using this to show the users amount of lives

--- spaceinvader.py
+++ spaceinvader.py
@@ -10,6 +10,15 @@ pg.display.set_caption("Space Invaders")
 clock = pg.time.Clock() # adds clock object to allow for the controlling of game speed
 
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+font_name = pg.font.match_font("arial")
+def draw_text(surf, text, size, x, y):
+  font = pg.font.Font(font_name, size)
+  text_surface = font.render(text,True, )
+  text_rect = text_surface.get_rect()
+  text_rect.midtop = (x , y)
+  surf.blit(text_surface, text_rect)
 
 
 
