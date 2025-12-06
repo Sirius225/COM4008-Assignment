6b276d4aa8cbf45dc361b69d36dd1860ed062da0
K.Calii <chikunichawakundai@gmail.com>
2025-12-06 18:29:20 +0000

    Changed line 340

--- spaceinvader.py
+++ spaceinvader.py
@@ -337,12 +337,38 @@ for invader, bullet_list in hits.items():
 
 # Barrier block class
 class Block(pg.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self,size,colour, x, y):
         super().__init__()
         self.image = pg.Surface((5, 5))
         self.image.fill((255, 0, 0)) # green
         self.rect = self.image.get_rect(topleft=(x, y))
+      
+shape = [
 
+"  xxxxxxx",
+" xxxxxxxxxx",
+"xxxxxxxxxxxxx",
+"xxxxxxxxxxxxx",
+"xxxxxxxxxxxxx",
+"xxx       xxx",
+"xx         xx"]   
+
+
+def make_barriers():
+    barriers = pg.sprite.Group()
+    barrier_x_positions = [30, 150, 270, 390, 510]
+    barrier_y = 450
     
+    for bx in barrier_x_positions:
+        for row_index, row in enumerate(shape):
+            for col_index, char in enumerate(row):
+                if char == "x":
+                    x = bx + col_index * 5
+                    y = barrier_y + row_index * 5
+                    block = Block(x, y)
+                    barriers.add(block)
+
+
+    return barriers
 
 pg.quit()
